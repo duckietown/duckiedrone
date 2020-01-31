0bf4b8e84fd883990c14eff87e36b5f178f5b6ee
Arthur MacKeith <amackeith@uchicago.edu>
2020-01-31 12:14:20 -0600

    trying to commit vision only

--- launch.sh
+++ launch.sh
@@ -12,10 +12,6 @@ dt_launchfile_init
 # NOTE: Use the variable CODE_DIR to know the absolute path to your code
 # NOTE: Use `dt_exec COMMAND` to run the main process (blocking process)
 
-# remap I2C
-if [ "${DEBUG}" = "1" ]; then echo "Remapping I2C..."; fi
-python2 $CODE_DIR/packages/pidrone_pkg/scripts/remap_i2c.py
-if [ "${DEBUG}" = "1" ]; then echo "Done!"; fi
 
 # launching app
 dt_exec roslaunch pidrone_pkg drone.launch
