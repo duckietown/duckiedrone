f6dae5b23a3f6c8bf3553d8a568dafb213e570dd
Arthur MacKeith <amackeith@uchicago.edu>
2020-03-04 20:22:11 -0600

    breaking up package

--- launch.sh
+++ launch.sh
@@ -13,7 +13,7 @@ dt_launchfile_init
 
 
 # launching app
-dt_exec roslaunch pidrone_pkg drone.launch veh:=$VEHICLE_NAME \
+dt_exec roslaunch non_hardware all.launch veh:=$VEHICLE_NAME \
 	robot_type:=$ROBOT_TYPE
 #this is where the node that communicates with the web browser is launched
 #dt_exec roslaunch pidrone_pkg rosbridge_websocket.launch
